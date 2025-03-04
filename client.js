3fe86cc7154ce06b3795aa1cdc2d2a1ed2d0f954
demon7x <demon7x@westworld.co.kr>
2025-03-04 18:51:39 +0900

    codec

--- client.js
+++ client.js
@@ -258,7 +258,7 @@ function recordVideo(duration, framerate, customCommand, onComplete,data) {
             console.log('exec error: ' + error);
         }
         console.log("recode complete");
-        console.log(data);
+        console.log(data.takeId);
         process.exit(sendVideo(getAbsoluteVideoPath(), data.takeId, data.cameraId));
     });
     // Process the customCommand to customize the arguments
